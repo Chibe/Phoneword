8c2544b36cc514e9a20a8976949ad5c4542e1007
Drew <Chibe1313@gmail.com>
2018-05-09 15:50:52 -0400

    Added second screen

--- Phoneword/Phoneword/MainActivity.cs
+++ Phoneword/Phoneword/MainActivity.cs
@@ -3,12 +3,16 @@ using Android.App;
 using Android.Content;
 using Android.Widget;
 using Android.OS;
+using System.Collections.Generic;
+using Core;
 
 namespace Phoneword
 {
     [Activity(Label = "Phone Word", MainLauncher = true)]
     public class MainActivity : Activity
     {
+        static readonly List<string> phoneNumbers = new List<string>();
+    
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -20,20 +24,31 @@ namespace Phoneword
             EditText phoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
             TextView translatedPhoneWord = FindViewById<TextView>(Resource.Id.TranslatedPhoneWord);
             Button translateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+            Button translationHistoryButton = FindViewById<Button>(Resource.Id.TranslationHistoryButton);
 
             // Add code to translate number
+            string translatedNumber = string.Empty;
             translateButton.Click += (sender, e) =>
             {
                 // Translate user's alphanumeric phone number to numeric
-                string translatedNumber = Core.PhonewordTranslator.ToNumber(phoneNumberText.Text);
+                translatedNumber = PhonewordTranslator.ToNumber(phoneNumberText.Text);
                 if (string.IsNullOrWhiteSpace(translatedNumber))
                 {
-                    translatedPhoneWord.Text = string.Empty;
+                    translatedPhoneWord.Text = "";
                 }
                 else
                 {
                     translatedPhoneWord.Text = translatedNumber;
+                    phoneNumbers.Add(translatedNumber);
+                    translationHistoryButton.Enabled = true;
                 }
+            };
+
+            translationHistoryButton.Click += (sender, e) =>
+            {
+                var intent = new Intent(this, typeof(TranslationHistoryActivity));
+                intent.PutStringArrayListExtra("phone_numbers", phoneNumbers);
+                StartActivity(intent);
             };
         }
     }
